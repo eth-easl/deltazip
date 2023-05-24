dc5ec5156dbc7e6d80b69f22c4735d8b643bace5
Xiaozhe Yao <askxzyao@gmail.com>
2023-05-24 01:33:28 +0000

    6.7b scripts

--- experiments/compress_nodelta.py
+++ experiments/compress_nodelta.py
@@ -3,15 +3,17 @@ import os
 tasks = [
     'answer_verification',
     'coherence_classification',
+    'commonsense_classification',
     'dialogue_state_tracking',
     'fact_verification',
     'gender_classification',
     'irony_detection',
+    'stance_detection',
     'toxic_language_detection',
     'word_semantics'
 ]
 
-model_size = 'opt-1.3b'
+model_size = 'opt-6.7b'
 
 wbits = [2,3,4]
 sparsities = [0]
@@ -22,7 +24,7 @@ for task in tasks:
             job = f"python cli/preset.py --base-model facebook/{model_size} --target-model .cache/models/{model_size}/{task} --dataset .cache/ni_calib/train/{task}.jsonl --wbit {wbit} --sparsity {sparsity} --out-dir .cache/compressed_models/{model_size}"
             jobs.append(job)
 
-os.system(f"ts -S 7")
+os.system(f"ts -S 32")
 
 for job in jobs:
     os.system(f"ts --gpus 1 {job}")
