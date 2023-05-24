8d0c7021a2ca6a26fcaef7dc4b6ae32d9970d3e4
Xiaozhe Yao <askxzyao@gmail.com>
2023-05-24 11:29:45 +0200

    scripts for 7-zipping compressed models used for calc real file sizes

--- experiments/eval_finetuned_gptq.py
+++ experiments/eval_finetuned_gptq.py
@@ -15,7 +15,7 @@ target_path = '.cache/compressed_models/opt-1.3b'
 
 os.system(f"ts -S 4")
 for task in tasks:
-    for delta in [x for x in os.listdir(target_path) if x.find(task) != -1 and x.find("delta") == -1]:
+    for delta in [x for x in os.listdir(target_path) if x.find(task) == -1 and x.find("delta") == -1]:
     # first evaluate base_model
         job = f"python cli/ni_eval_gptq.py --base-model {base_model} --task {task} --target-model {target_path}/{delta}"
         os.system(f"ts --gpus 1 {job}")
