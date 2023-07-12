b9d44e149d8256b9cdced9332920f79d0ecaf1d6
Xiaozhe Yao <askxzyao@gmail.com>
2023-07-12 15:56:44 +0200

    cleaning

--- csrc/nvcomp/install-deps.sh
+++
@@ -1,6 +0,0 @@
-# conda install -c anaconda cmake
-# conda install -c "nvidia/label/cuda-11.8.0" cuda-nvcc
-# conda install -c "nvidia/label/cuda-11.8.0" cuda-toolkit
-# conda install -c "nvidia/label/cuda-11.8.0" cuda-runtime
-sudo apt install zlib1g liblz4-dev zlib1g-dev libdeflate-dev libboost-all-dev -y
-conda install -c bioconda libdeflate
