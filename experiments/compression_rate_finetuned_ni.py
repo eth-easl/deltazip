f0be3fc16fd86e9a636f3d76a1b7be6a84e03af1
Xiaozhe Yao <askxzyao@gmail.com>
2023-07-28 15:19:06 +0200

    cleaning & restructure

--- experiments/compression_rate_finetuned_ni.py
+++ experiments/compression_rate_finetuned_ni.py
@@ -1,5 +1,15 @@
 import os
-models = ["fmzip/pythia-2.8b-deduped-sentence_ordering"]
 
-out_dir = os.environ.get("YAO_CACHE", ".cache")
+in_dir = os.environ.get("YAO_CACHE", "experiments", "fmzip", "finetuned_raw", "pythia-2.8b-deduped")
+out_dir = os.environ.get("YAO_CACHE", "experiments", "fmzip", "finetuned_compressed", "pythia-2.8b-deduped")
 
+BASE_MODEL = 'EleutherAI/pythia-2.8b-deduped'
+
+os.makedirs(out_dir, exist_ok=True)
+delta_ops = ['subtract']
+
+tasks = os.listdir(in_dir)
+
+for task in tasks:
+    steps = os.listdir(os.path.join(in_dir, task))
+    
