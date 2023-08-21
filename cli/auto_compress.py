6b89f4d61cda10b8da87d6dffaf5545f8899a01a
Xiaozhe Yao <askxzyao@gmail.com>
2023-08-21 15:10:14 +0200

    minor

--- cli/auto_compress.py
+++ cli/auto_compress.py
@@ -10,10 +10,10 @@ from fmzip.utils.delta_utils import subtract, xor
 def main(args):
     print(args)
     tokenizer = AutoTokenizer.from_pretrained(args.target_model, use_fast=True)
-    
     compress_config = AutoCompressionConfig(
-        bits = [2,3],
-        sparsity=[0.5, 0.9],
+        tolerance=args.tolerance,
+        bits = [2, 3, 4],
+        sparsity=[0.5,0.75,0.9, 0.99],
         prunen=args.prunen,
         prunem=args.prunem,
         lossless=args.lossless,
@@ -68,8 +68,7 @@ if __name__=="__main__":
     parser.add_argument("--dataset", type=str, default="answer_verification", help="The dataset to use for training, must be a path to a jsonl file.")
     parser.add_argument("--n-samples", type=int, default=1024, help="How many data samples used for calibration, -1 means all.")
     parser.add_argument("--target-model", type=str, default="facebook/opt-125m")
-    parser.add_argument("--sparsity", type=float, default=0.5)
-    parser.add_argument("--bits", type=int, default=4)
+    parser.add_argument("--tolerance", type=float, default=1e-9)
     parser.add_argument("--group-size", type=int, default=-1)
     parser.add_argument("--prunen", type=int, default=0)
     parser.add_argument("--prunem", type=int, default=0)
