2c9391a1e3c9eba6c923437df5e3cdd9a2655006
Xiaozhe Yao <askxzyao@gmail.com>
2023-08-29 11:28:21 +0200

    wip: faster evaluation

--- benchmarks/model_quality/ni_generate_all.py
+++ benchmarks/model_quality/ni_generate_all.py
@@ -2,12 +2,12 @@ import os
 
 CACHE_PATH = os.environ.get("YAO_CACHE", "")
 
-compressed_models = os.path.join(CACHE_PATH, "experiments/fmzip/compressed_models/auto/tol_1e-9/pythia-2.8b-deduped")
+compressed_models = os.path.join(CACHE_PATH, "experiments/fmzip/compressed_models/auto/pythia-2.8b-deduped")
 
 data_dir = os.path.join(CACHE_PATH, "datasets/qi/test")
 
 tasks = os.listdir(compressed_models)
-output_dir = os.path.join(CACHE_PATH, "experiments/fmzip/generation_results")
+output_dir = os.path.join(CACHE_PATH, "experiments/fmzip/generation_results_0.6")
 jobs = []
 for task in tasks:
     steps = os.listdir(os.path.join(compressed_models, task))
@@ -17,7 +17,7 @@ for task in tasks:
         test_datafile = os.path.join(data_dir, task+".test.jsonl")
         job = f"python cli/ni_generate.py --base-model EleutherAI/pythia-2.8b-deduped --target-model {os.path.join(compressed_models, task, step)} --delta subtract --input-file {test_datafile} --input-field input --max-length 64 --output-file {os.path.join(CACHE_PATH, 'experiments/fmzip/generation_results', task, step)}.jsonl"
         jobs.append(job)
-os.system("TS_VISIBLE_DEVICES=1,2,3 ts -S 3")
+os.system("TS_VISIBLE_DEVICES=1,2,3 ts -S 4")
 for job in jobs:
     os.system(f"TS_VISIBLE_DEVICES=1,2,3 ts --gpus 1 {job}")
     # os.system(f"{job}")
