529c30bf3127fd03d4499fef1dcc0027d5eb72f7
Xiaozhe Yao <askxzyao@gmail.com>
2023-09-17 00:06:48 +0200

    cuda impl for quant forward

--- examples/hybrid_generation.py
+++ examples/hybrid_generation.py
@@ -3,3 +3,4 @@ from fmzip.pipelines import TextGenerationPipeline
 
 pipeline = TextGenerationPipeline("EleutherAI/pythia-2.8b-deduped")
 pipeline.load_delta(".cache/compressed_models/p2.8b_gsd_133")
+
