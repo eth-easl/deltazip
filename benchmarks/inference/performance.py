d3e4b139f1ab748d159bf3e0b7170bb1b6285a8f
Xiaozhe Yao <askxzyao@gmail.com>
2023-09-20 16:41:00 +0200

    minor

--- benchmarks/inference/performance.py
+++ benchmarks/inference/performance.py
@@ -1,9 +1,19 @@
 import requests
-
+from multiprocessing import Pool
 endpoint = 'http://localhost:8000'
 
 response_time = []
-for i in range(10):
-    res = requests.get(endpoint + '/inference')
-    print(res.headers)
-    print(res.json())
+# rewrite the following with multi threading
+
+def test(i):
+    print(f"issuing {i}th request")
+    task = {
+        'prompt': "Once upon a time, ",
+        'model': 'gpt2'
+    }
+    res = requests.post(endpoint + '/inference', json=task)
+    return res.json()
+
+with Pool(16) as p:
+    results = p.map(test, range(16))
+    print(results)
--- fmzip/rest/server.py
+++ fmzip/rest/server.py
@@ -1,36 +1,64 @@
+import json
 import time
 import asyncio
+import hashlib
+from typing import Optional
 from typing import Callable
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.routing import APIRoute
 from timeit import default_timer as timer
 from fastapi import FastAPI, Request, Response
+from fmzip.rest.task_queue import TaskQueueWithTimeout
 
-class LockableRoute(APIRoute):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.lock = asyncio.Lock()
+batch_size = 4
+timeout = 3
+task_queue = asyncio.Queue()
 
-    def get_route_handler(self) -> Callable:
-        original_route_handler = super().get_route_handler()
-        async def custom_route_handler(request: Request) -> Response:
-            start = timer()
-            await self.lock.acquire()
-            response: Response = await original_route_handler(request)
-            self.lock.release()
-            end = timer()
-            response.headers["X-Response-Time"] = str(end-start)
-            return response
+async def process_tasks():
+    while True:
+        batch = []
+        try:
+            # Try to collect batch_size number of tasks within timeout seconds
+            for _ in range(batch_size):
+                task = await asyncio.wait_for(task_queue.get(), timeout=timeout)
+                batch.append(task)
+        except asyncio.TimeoutError:
+            # If timeout, get whatever tasks are available
+            while not task_queue.empty():
+                task = await task_queue.get()
+                batch.append(task)
 
-        return custom_route_handler
+        if len(batch) > 0:
+            print(f"Processing batch: {batch}")
+            for task in batch:
+                print(f"Executing task: {task}")
+                results[task]['result'] = f"{task} completed"
+                results[task]['event'].set()
+                await asyncio.sleep(0.2)  # Simulate task execution
+            print("Batch processing completed.\n")
 
+# Run the processing function as a background task
+asyncio.create_task(process_tasks())
 
+dhash = hashlib.md5()
+results = {}
+
+class InferenceTask(BaseModel):
+    id: Optional[str]=""
+    prompt: str
+    model: str
+    response: Optional[str]=""
 
 app = FastAPI()
-app.router.route_class = LockableRoute
 
-@app.get("/inference")
-async def handle_request():
-    time.sleep(0.5)
-    return {"message": 'done'}
+@app.post("/inference", response_model=InferenceTask)
+async def handle_request(inference_task: InferenceTask):
+    dhash.update(json.dumps(inference_task.model_dump(), sort_keys=True).encode())
+    inference_task.id = dhash.hexdigest()
+    event = asyncio.Event()
+    results[inference_task.id] = {'event': event, 'result': None}
+    await task_queue.put(inference_task.id)
+    await event.wait()
+    inference_task.response = results.pop(inference_task.id)['result']
+    return inference_task
