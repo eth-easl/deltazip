e43fe4e06bd628c09b90c416b1f3e59954684ef4
Xiaozhe Yao <askxzyao@gmail.com>
2023-09-25 23:42:29 +0200

    fix conversion format

--- scripts/utils/convert_chat_dataset.py
+++ scripts/utils/convert_chat_dataset.py
@@ -4,12 +4,12 @@ import argparse
 def convert(format):
     if format=='meta':
         user_identifier = "[INST]"
-        bot_identifier = "<<SYS>>\n"
+        bot_identifier = ""
         user_end = "[/INST]"
-        bot_end = "\n<</SYS>>\n\n"
+        bot_end = "</s>"
     elif format == 'lmsys':
-        user_identifier = "USER"
-        bot_identifier = "ASSISTANT"
+        user_identifier = "USER:"
+        bot_identifier = "ASSISTANT:"
         user_end = " "
         bot_end = "</s>"
     with open(".cache/datasets/ShareGPT_V3_unfiltered_cleaned_split.json", "r") as fp:
@@ -19,9 +19,9 @@ def convert(format):
         output_text = ""
         for conv in datum['conversations']:
             if conv['from'] == "human":
-                output_text += user_identifier + ": " + conv['value'] +user_end
+                output_text += user_identifier + " " + conv['value'] +user_end
             elif conv['from'] == "gpt":
-                output_text += bot_identifier + ": " + conv['value'] + bot_end
+                output_text += bot_identifier + " " + conv['value'] + bot_end
         outputs.append(output_text)
 
     with open(f".cache/datasets/{format}.jsonl", "w") as fp:
@@ -32,3 +32,4 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--format", type=str, choices=['lmsys', 'meta'], default='lmsys')
     args = parser.parse_args()
+    convert(args.format)
