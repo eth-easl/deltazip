0dca0b1aa810c012c7a3174435db7bb0ff62cd78
Xiaozhe Yao <askxzyao@gmail.com>
2023-09-28 02:19:52 +0200

    minor

--- fmzip/rest/server.py
+++ fmzip/rest/server.py
@@ -69,7 +69,10 @@ class RestartRequest(BaseModel):
 @app.post("/inference", response_model=InferenceTask)
 async def handle_request(inference_task: InferenceTask):
     print("Received request")
-    dhash.update(json.dumps(inference_task.model_dump(), sort_keys=True).encode())
+    try:
+        dhash.update(json.dumps(inference_task.model_dump(), sort_keys=True).encode())
+    except:
+        dhash.update(json.dumps(inference_task.dict(), sort_keys=True).encode())
     inference_task.id = dhash.hexdigest()
     event = asyncio.Event()
     results[inference_task.id] = {'event': event, 'result': None}
