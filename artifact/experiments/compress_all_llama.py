4989ae2b4813ed90f63449fd657f2e891d826098
Xiaozhe Yao <askxzyao@gmail.com>
2023-10-10 15:34:49 +0200

    cleanup structure

--- artifact/experiments/compress_all_llama.py
+++ artifact/experiments/compress_all_llama.py
@@ -20,8 +20,9 @@ jobs = []
 for task in tasks:
     steps = os.listdir(os.path.join(in_folder, task))
     for step in steps:
-        job = f"python cli/compress.py --target-model {os.path.join(in_folder, task, step)} --outdir {os.path.join(out_dir, task, step)} --dataset {os.path.join(ar_dataset, task+'.train.jsonl')} --n-samples 256 --bits 2 --group-size 128 --sparsity 0.75 --lossless gdeflate --delta subtract --base-model openlm-research/open_llama_3b_v2"
-        jobs.append(job)
+        if not os.path.exists(os.path.join(out_dir, task, step, "config.json")):
+            job = f"python cli/compress.py --target-model {os.path.join(in_folder, task, step)} --outdir {os.path.join(out_dir, task, step)} --dataset {os.path.join(ar_dataset, task+'.train.jsonl')} --n-samples 256 --bits 2 --group-size 128 --sparsity 0.75 --lossless gdeflate --delta subtract --base-model openlm-research/open_llama_3b_v2"
+            jobs.append(job)
 
 os.system("ts -S 4")
 for job in jobs:
