c5898a085cc19aae4807c93da72e30c61c791b9f
Xiaozhe Yao <askxzyao@gmail.com>
2023-10-15 19:15:58 +0200

    update plotting

--- artifact/plots/plot_micro_benchmark_single.py
+++ artifact/plots/plot_micro_benchmark_single.py
@@ -12,9 +12,9 @@ def plot(args):
         backend = item['backend']
         backend_name = backend['name']
         if backend_name == "hf":
-            backend_name = f"{backend_name}, bsz={backend['args']['batch_size']}, max_models={backend['args']['max_num_models']}"
+            backend_name = f"{backend_name}<br>bsz={backend['args']['batch_size']}<br>max_models={backend['args']['max_num_models']}"
         elif backend_name == "fmzip":
-            backend_name = f"{backend_name}, bsz={backend['args']['batch_size']}, max_deltas={backend['args']['max_num_deltas']}, strategy={backend['args']['placement_strategy']}, lossless_only={backend['args']['lossless_only']}"
+            backend_name = f"{backend_name}<br>bsz={backend['args']['batch_size']}<br>max_deltas={backend['args']['max_num_deltas']}<br>strategy={backend['args']['placement_strategy']}<br>lossless_only={backend['args']['lossless_only']}"
         
         min_length = item['gen_args']['min_length']
         for idx, query in enumerate(item['results']):
@@ -24,14 +24,7 @@ def plot(args):
             loading_time = query['measure']['loading_time']
             prepare_time = query['measure']['prepare_time']
             inference_time = query['measure']['inference_time']
-            # waiting_time = total_elapsed - (tokenize_time + loading_time + prepare_time + inference_time)
-            # plot_data.append({
-            #     "id": idx,
-            #     "backend": backend_name,
-            #     "tokens": min_length,
-            #     "time_elapsed": waiting_time,
-            #     "Breakdown": "Others",
-            # })
+            total_time = tokenize_time + loading_time + prepare_time + inference_time
             plot_data.append({
                 "id": idx,
                 "backend": backend_name,
@@ -61,11 +54,27 @@ def plot(args):
                 "Breakdown": "Generation",
             })
     df = pd.DataFrame(plot_data)
-    print(df)
-    fig = px.bar(df, x="backend", y="time_elapsed", facet_col="id", color="Breakdown")
+    # reverse the order of the dataframe
+    df = df.iloc[::-1]
+
+    fig = px.bar(df, x="backend", y="time_elapsed", color="Breakdown")
     fig.update_layout(
-        width=800, height=600, title_x=0.5, title_text="Breakdown of Latency (s)"
+        width=800, height=600, title_x=0.5, title_text="Breakdown of Latency (s)<br>Single Query; 1x RTX 3090; 2.1 GB/s"
     )
+    fig.update_layout(
+        font_family="Arial",
+        font_color="black",
+        title_font_family="Arial",
+        title_font_color="black",
+        legend_title_font_color="black"
+    )
+    fig.update_layout(
+        title=dict(font=dict(size=20)),
+        legend = dict(font = dict(size = 18)),
+        legend_title = dict(font = dict(size = 18)),
+    )
+    fig.update_yaxes(title_text="Time Elapsed", title_font=dict(size=22), tickfont_size=18)
+    fig.update_xaxes(title_text="Backend", title_font=dict(size=22), tickfont_size=14)
     fig.write_image(args.output, scale=2)
 
 if __name__=="__main__":
