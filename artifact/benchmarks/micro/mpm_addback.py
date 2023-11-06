abe28b60f5422bed638599606bdc0f9ba3500e2c
Xiaozhe Yao <askxzyao@gmail.com>
2023-11-06 11:37:29 +0100

    add microbenchmark for colocating vs. adding back

---
+++ artifact/benchmarks/micro/mpm_addback.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from timeit import default_timer as timer
+from fmzip.pipelines import FMZipPipeline
+from loguru import logger
+
+logger.remove()
+logger.add(sys.stdout, level="WARNING")
+
+base_model = "openlm-research/open_llama_3b_v2"
+
+requests = [
+    ("Computer Science is about ", ".cache/compressed_models/3b-parameters/openllama-chat-1"),
+    ("Alan Turing is ", ".cache/compressed_models/3b-parameters/openllama-chat-2"),
+    ("Von Neumann is ", ".cache/compressed_models/3b-parameters/openllama-chat-3"),
+    ("QED is ", ".cache/compressed_models/3b-parameters/openllama-chat-4"),
+    ("QED is ", ".cache/compressed_models/3b-parameters/openllama-chat-5"),
+    ("QED is ", ".cache/compressed_models/3b-parameters/openllama-chat-6"),
+    ("QED is ", ".cache/compressed_models/3b-parameters/openllama-chat-7"),
+    ("QED is ", ".cache/compressed_models/3b-parameters/openllama-chat-8"),
+]
+
+def addback():
+    pipeline = FMZipPipeline(
+        base_model=base_model,
+        max_num_deltas=1,
+        batch_size=1,
+        placement_strategy='addback',
+        offload_base_model=True,
+    )
+    return pipeline.generate(requests)
+
+def colocate():
+    pipeline = FMZipPipeline(
+        base_model=base_model,
+        max_num_deltas=8,
+        batch_size=5,
+        placement_strategy='colocate'
+    )
+    return pipeline.generate(requests)
+
+def benchmark():
+    start = timer()
+    addback()
+    end = timer()
+    print(f"[Addback]: {end - start:.2f}s")
+
+    start = timer()
+    colocate()
+    end = timer()
+    print(f"[Colocate]: {end - start:.2f}s")
+
+if __name__ == "__main__":
+    benchmark()
