5d0caf5781d7a630d86f37bc5390fc44f83a8be6
Xiaozhe Yao <askxzyao@gmail.com>
2023-11-13 17:06:39 +0100

    minor

--- artifact/compress_experiments/compress_all_llama.py
+++ artifact/compress_experiments/compress_all_llama.py
@@ -2,11 +2,11 @@ import os
 
 cache_folder = os.environ.get("YAO_CACHE")
 bits = 4
-sparsity = 0
+sparsity = 0.5
 in_folder = os.path.join(
     cache_folder, "experiments", "fmzip", "finetuned_raw", "llama-3b"
 )
-
+include_nodelta = True
 poi_tasks = [
     'task151_tomqa_find_location_easy_clean',
     'task152_tomqa_find_location_easy_noise',
@@ -14,11 +14,19 @@ poi_tasks = [
     'task936_defeasible_nli_snli_classification', 
     'task380_boolq_yes_no_question', 
     'task1308_amazonreview_category_classification', 
-    'task515_senteval_odd_word_out', 
     'task372_synthetic_palindrome_numbers', 
     'task227_clariq_classification'
 ]
-
+poi_steps = [
+    '140',
+    '160',
+    '120',
+    '120',
+    '270',
+    '150',
+    '105',
+    '75',
+]
 out_dir = os.path.join(
     cache_folder,
     "experiments",
@@ -31,14 +39,17 @@ ar_dataset = os.path.join(cache_folder, "datasets", "qi", "ar")
 tasks = os.listdir(in_folder)
 
 jobs = []
-for task in tasks:
-    if task in poi_tasks:
-        steps = os.listdir(os.path.join(in_folder, task))
-        for step in steps:
+for i, task in enumerate(poi_tasks):
+    steps = os.listdir(os.path.join(in_folder, task))
+    for step in steps:
+        if poi_steps[i] in step:
             if not os.path.exists(os.path.join(out_dir, task, step, "config.json")):
                 job = f"python cli/compress.py --target-model {os.path.join(in_folder, task, step)} --outdir {os.path.join(out_dir, task, step)} --dataset {os.path.join(ar_dataset, task+'.train.jsonl')} --n-samples 512 --bits {bits} --group-size 128 --sparsity {sparsity} --lossless gdeflate --delta subtract --base-model openlm-research/open_llama_3b_v2"
                 jobs.append(job)
+                if include_nodelta:
+                    job = f"python cli/compress.py --target-model {os.path.join(in_folder, task, step)} --outdir {os.path.join(out_dir+'_nodelta', task, step)} --dataset {os.path.join(ar_dataset, task+'.train.jsonl')} --n-samples 512 --bits {bits} --group-size 128 --sparsity {sparsity} --lossless gdeflate --base-model openlm-research/open_llama_3b_v2"
+                    jobs.append(job)
 
-os.system("ts -S 3")
+os.system("ts -S 4")
 for job in jobs:
     os.system(f"ts --gpus 1 {job}")
