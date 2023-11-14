1ed41894164a97753552b98dd8e0edac4551f70b
Xiaozhe Yao <askxzyao@gmail.com>
2023-11-14 11:52:34 +0100

    allow handling of smaller batch during bmm

--- fmzip/nn_modules/triton_utils/custom_autotune.py
+++ fmzip/nn_modules/triton_utils/custom_autotune.py
@@ -9,7 +9,6 @@ from typing import Dict
 Mostly the same as the autotuner in Triton, but with a few changes like using 40 runs instead of 100.
 """
 
-
 class CustomizedTritonAutoTuner(triton.KernelInterface):
     def __init__(
         self,
@@ -218,6 +217,72 @@ def matmul248_kernel_config_pruner(configs, nargs):
                 "BLOCK_SIZE_N": block_size_n,
                 "BLOCK_SIZE_K": block_size_k,
                 "GROUP_SIZE_M": group_size_m,
+            },
+            num_stages=config.num_stages,
+            num_warps=config.num_warps,
+        )
+
+def autotune(
+    configs, key, prune_configs_by=None, reset_to_zero=None, nearest_power_of_two=False
+):
+    def decorator(fn):
+        return CustomizedTritonAutoTuner(
+            fn,
+            fn.arg_names,
+            configs,
+            key,
+            reset_to_zero,
+            prune_configs_by,
+            nearest_power_of_two,
+        )
+
+    return decorator
+
+
+def bmm248_kernel_config_pruner(configs, nargs):
+    """
+    The main purpose of this function is to shrink BLOCK_SIZE_* when the corresponding dimension is smaller.
+    """
+    m = max(2 ** int(math.ceil(math.log2(nargs["M"]))), 16)
+    n = max(2 ** int(math.ceil(math.log2(nargs["N"]))), 16)
+    k = max(2 ** int(math.ceil(math.log2(nargs["K"]))), 16)
+
+    used = set()
+    for config in configs:
+        block_size_m = min(m, config.kwargs["BLOCK_SIZE_M"])
+        block_size_n = min(n, config.kwargs["BLOCK_SIZE_N"])
+        block_size_k = min(k, config.kwargs["BLOCK_SIZE_K"])
+        group_size_m = config.kwargs["GROUP_SIZE_M"]
+        batch_size_b = config.kwargs["BATCH_SIZE_B"]
+        if (
+            block_size_m,
+            block_size_n,
+            block_size_k,
+            group_size_m,
+            config.num_stages,
+            config.num_warps,
+            batch_size_b,
+        ) in used:
+            continue
+
+        used.add(
+            (
+                block_size_m,
+                block_size_n,
+                block_size_k,
+                group_size_m,
+                batch_size_b,
+                config.num_stages,
+                config.num_warps,
+            )
+        )
+        yield triton.Config(
+            {
+                "BLOCK_SIZE_M": block_size_m,
+                "BLOCK_SIZE_N": block_size_n,
+                "BLOCK_SIZE_K": block_size_k,
+                "GROUP_SIZE_M": group_size_m,
+                "BATCH_SIZE_B": batch_size_b,
             },
             num_stages=config.num_stages,
             num_warps=config.num_warps,
