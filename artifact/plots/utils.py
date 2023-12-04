1e2b0b5deffb09cea3533aed59c1e509122c62d0
Xiaozhe Yao <askxzyao@gmail.com>
2023-12-04 14:45:47 +0100

    add results

--- artifact/plots/utils.py
+++ artifact/plots/utils.py
@@ -1,12 +1,11 @@
-
-
 strategy_mapping = {"none": "None", "addback": "AS", "colocate": "MMA"}
+project_name="FiniCompress"
 
 def get_provider_name(provider):
     if provider["name"] == "hf":
         return "HuggingFace"
     elif provider["name"] == "fmzip":
-        return f"DeltaZip, bsz={provider['args'].get('batch_size', 1)}<br>{strategy_mapping[provider['args'].get('placement_strategy','none')]} {'Lossless' if provider['args'].get('lossless_only', False) else 'Lossy'}"
+        return f"{project_name}<br>bsz={provider['args'].get('batch_size', 1)}<br>{strategy_mapping[provider['args'].get('placement_strategy','none')]}, {'Lossless' if provider['args'].get('lossless_only', False) else 'Lossy'}"
 
 def get_provider_order(provider):
     if provider["name"] == "hf":
