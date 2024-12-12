5afce3b34279b54b2ea08ebcb40a7259df6a05f1
Xiaozhe Yao <askxzyao@gmail.com>
2024-12-12 11:43:03 +0100

    cleanup

--- meta/scripts/eval.py
+++ meta/scripts/eval.py
@@ -1,12 +1,17 @@
 import os
 
-tasks = ['lambada_openai','arc_easy','boolqa','logiqa','openbookqa', 'piqa','truthfulqa']
+tasks = ['mmlu','hellaswag','winogrande','arc_challenge']
 tasks = ','.join(tasks)
 
 merged_models = os.listdir('.local/merged_models')
 
-
 for model in merged_models:
     path = os.path.join('.local/merged_models', model)
-    job = f"lm_eval --model vllm --model_args pretrained={path},dtype=auto,tensor_parallel_size=1 --tasks {tasks} --use_cache .local/cache/{model} --output_path .local/eval_results/{model} --batch_size auto"
+    job = f"lm_eval --model hf --model_args pretrained={path},dtype=auto --tasks {tasks} --use_cache .local/cache/{model} --output_path .local/eval_results/{model} --batch_size auto"
+    os.system(f"ts -G 1 {job}")
+
+full_models = ['google/gemma-2-9b-it', 'google/gemma-2-9b', 'google/gemma-2-2b-it', 'google/gemma-2-2b', 'meta-llama/Llama-3.2-1B', 'meta-llama/Llama-3.2-3B', 'meta-llama/Llama-3.2-1B-Instruct', 'meta-llama/Llama-3.2-3B-Instruct']
+
+for model in full_models:
+    job = f"lm_eval --model hf --model_args pretrained={model},dtype=auto --tasks {tasks} --use_cache .local/cache/{model} --output_path .local/eval_results/{model} --batch_size auto"
     os.system(f"ts -G 1 {job}")
